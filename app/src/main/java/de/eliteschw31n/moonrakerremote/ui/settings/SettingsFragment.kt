0159fff792d6a31483d580c8fb097a6d9fe38ac4
eliteSCHW31N <business@eliteschw31n.de>
2021-04-13 13:44:31 +0200

    remove useless class

--- app/src/main/java/de/eliteschw31n/moonrakerremote/ui/settings/SettingsFragment.kt
+++ app/src/main/java/de/eliteschw31n/moonrakerremote/ui/settings/SettingsFragment.kt
@@ -12,24 +12,13 @@ import de.eliteschw31n.moonrakerremote.R
 
 class SettingsFragment : Fragment() {
 
-    private lateinit var settingsMenuViewModel: SettingsViewModel
-
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        settingsMenuViewModel =
-                ViewModelProvider(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        //val printerWebsocket: TextView = root.findViewById(R.id.input_settings_connection_websocket)
-        //settingsMenuViewModel.printerWebsocket.observe(viewLifecycleOwner, Observer {
-         //   printerWebsocket.text = it
-        //})
-        //val printerStreamURL: TextView = root.findViewById(R.id.input_settings_connection_stream_url)
-        //settingsMenuViewModel.printerStreamURL.observe(viewLifecycleOwner, Observer {
-        //    printerStreamURL.text = it
-        //})
+
         return root
     }
 }
