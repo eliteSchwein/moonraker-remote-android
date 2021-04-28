a0828d58b6aff3b0b25e4a6891e63e314c8124c7
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-04-28 14:03:48 +0200

    prepare for proper profiles

--- app/src/main/java/de/eliteschw31n/moonrakerremote/ui/printer_menu/PrinterMenuFragment.kt
+++ app/src/main/java/de/eliteschw31n/moonrakerremote/ui/printer_menu/PrinterMenuFragment.kt
@@ -1,6 +1,7 @@
 package de.eliteschw31n.moonrakerremote.ui.printer_menu
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,9 +23,8 @@ import kotlin.random.Random
 class PrinterMenuFragment : Fragment() {
     private lateinit var printerProfiles: JSONObject
     private lateinit var currentPrinter: String
-    private lateinit var printerSelection: ConstraintLayout
     private lateinit var profilesLayout: FrameLayout
-    private var lastButton: Int = 0
+    private var lastProfile: String = ""
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -36,10 +36,7 @@ class PrinterMenuFragment : Fragment() {
         printerProfiles = LocalDatabase.getData().getJSONObject("printers")
         currentPrinter = LocalDatabase.getData().getString("currentPrinter")
 
-        printerSelection = root.findViewById(R.id.printer_menu_profile_layout)
-
         profilesLayout = root.findViewById(R.id.printer_menu_profiles_layout)
-        //loadPrinterProfileButtons()
         loadPrinterProfiles()
 
         val addButton: FloatingActionButton = root.findViewById(R.id.printer_menu_add)
@@ -49,102 +46,33 @@ class PrinterMenuFragment : Fragment() {
             defaultData.put("webcamurl", "http://mainsailos.local/webcam/?action=stream")
             val profileName = generateName()
             LocalDatabase.updatePrinterData(profileName, defaultData)
-            addPrinterProfileButton(profileName)
+            addPrinterProfile(profileName)
         }
         return root
     }
-    private fun handleProfileEditButton(printerEditButton: ImageButton) {
-        printerEditButton.setOnClickListener {
-            val database = LocalDatabase.getData()
-            val tag = printerEditButton.tag.toString()
-            database.put("currentPrinter", tag.replace("printer_edit_", ""))
-            findNavController().navigate(R.id.nav_printer_settings)
-        }
-    }
-    private fun handleProfileButton(printerButton: Button) {
-        printerButton.setOnClickListener {
-            val database = LocalDatabase.getData()
-            val tag = printerButton.tag.toString()
-            database.put("currentPrinter", tag.replace("printer_select_", ""))
-            val oldSelectedProfile: Button = printerSelection.findViewWithTag("printer_select_$currentPrinter")
-            oldSelectedProfile.isEnabled = true
-            currentPrinter = tag.replace("printer_select_", "")
-            LocalDatabase.writeData(database)
-            NavTitles.updateTitles()
-            printerButton.isEnabled = false
-        }
-    }
-    private fun loadPrinterProfileButtons() {
-        lastButton = 0
-        MainActivity.runUiUpdate(Runnable {
-            val printers = LocalDatabase.getData().getJSONObject("printers")
-            for (printer in printers.keys()){
-                addPrinterProfileButton(printer)
-            }
-        })
-    }
     private fun loadPrinterProfiles() {
-        lastButton = 0
-        MainActivity.runUiUpdate(Runnable {
-            val printers = LocalDatabase.getData().getJSONObject("printers")
-            for (printer in printers.keys()){
+        lastProfile = ""
+        Thread {
+            Thread.sleep(250)
+            for (printer in printerProfiles.keys()){
                 addPrinterProfile(printer)
+                Thread.sleep(100)
             }
-        })
+        }.start()
     }
     private fun addPrinterProfile(name: String) {
-        var printerProfile = PrinterSelect()
-        val profileData = LocalDatabase.getPrinterData(name)
-        var fragmentTransaction = MainActivity.supportFragmentManager().beginTransaction()
-        fragmentTransaction.add(R.id.printer_menu_profiles_layout, printerProfile)
+        val printerProfile = PrinterSelect()
+        val fragmentTransaction = MainActivity.supportFragmentManager().beginTransaction()
+        fragmentTransaction.add(R.id.printer_menu_profiles_layout, printerProfile, "printer_profile_$name")
         fragmentTransaction.commit()
-    }
-    private fun addPrinterProfileButton(name: String) {
-        val printerButton = Button(context)
-        printerButton.text = name
-        printerButton.id = View.generateViewId()
-        printerButton.tag = "printer_select_$name"
-        val layoutParams =
-                LinearLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.MATCH_PARENT,
-                        RelativeLayout.LayoutParams.WRAP_CONTENT)
-        printerButton.layoutParams = layoutParams
-        if(currentPrinter == name){
-            printerButton.isEnabled = false
+        while (printerProfile.view == null) {
+            Thread.sleep(100)
         }
-        printerSelection.addView(printerButton)
-        val constrainSet = ConstraintSet()
-        constrainSet.clone(printerSelection)
-        if(lastButton != 0) {
-            constrainSet.connect(printerButton.id, ConstraintSet.TOP, lastButton, ConstraintSet.BOTTOM, 16)
-        }
-        constrainSet.connect(printerButton.id, ConstraintSet.LEFT, R.id.printer_settings_activity, ConstraintSet.LEFT, 32)
-        constrainSet.connect(printerButton.id, ConstraintSet.RIGHT, R.id.printer_settings_activity, ConstraintSet.RIGHT, 32)
-        lastButton = printerButton.id
-        addPrinterProfileEditButton(name)
-        constrainSet.applyTo(printerSelection)
-        handleProfileButton(printerButton)
+        val profileView = printerProfile.view
+        val profileTitle: TextView = profileView!!.findViewById(R.id.component_printer_select_title)
+        profileTitle.text = name
     }
-    private fun addPrinterProfileEditButton(name: String) {
-        val printerEditButton = ImageButton(context)
-        printerEditButton.setImageResource(R.drawable.ic_settings_edit)
-        printerEditButton.setBackgroundResource(R.color.transparent)
-                printerEditButton.id = View.generateViewId()
-        printerEditButton.tag = "printer_edit_$name"
-        val layoutParams =
-                LinearLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.WRAP_CONTENT,
-                        RelativeLayout.LayoutParams.MATCH_PARENT)
-        printerEditButton.layoutParams = layoutParams
-        printerSelection.addView(printerEditButton)
-        val constrainSet = ConstraintSet()
-        constrainSet.clone(printerSelection)
-        constrainSet.connect(printerEditButton.id, ConstraintSet.RIGHT, lastButton, ConstraintSet.RIGHT, 32)
-        constrainSet.connect(printerEditButton.id, ConstraintSet.TOP, lastButton, ConstraintSet.TOP, 0)
-        constrainSet.connect(printerEditButton.id, ConstraintSet.BOTTOM, lastButton, ConstraintSet.BOTTOM, 0)
-        constrainSet.applyTo(printerSelection)
-        handleProfileEditButton(printerEditButton)
-    }
+
     private fun generateName(): String {
         val generatedName = "profile" + Random.nextInt(0, 1000)
         printerProfiles.keys().forEach {
